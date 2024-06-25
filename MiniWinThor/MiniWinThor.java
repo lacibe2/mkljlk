5b00b777abca48efdba17603e4f0a28e51802775
lacibe2 <lara27cibelly@gmail.com>
2024-06-25 00:43:11 -0300

    implementação basica

---
+++ MiniWinThor/MiniWinThor.java
@@ -0,0 +1,48 @@
+public class MiniWinThor {
+
+        private double codigo;
+        private String descricao;
+        private double precoDeCompra;
+        private double precoDeVenda;
+        private String statusDoProduto;
+
+    public double getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(double codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPrecoDeCompra() {
+        return precoDeCompra;
+    }
+
+    public void setPrecoDeCompra(double precoDeCompra) {
+        this.precoDeCompra = precoDeCompra;
+    }
+
+    public double getPrecoDeVenda() {
+        return precoDeVenda;
+    }
+
+    public void setPrecoDeVenda(double precoDeVenda) {
+        this.precoDeVenda = precoDeVenda;
+    }
+
+    public String getStatusDoProduto() {
+        return statusDoProduto;
+    }
+
+    public void setStatusDoProduto(String statusDoProduto) {
+        this.statusDoProduto = statusDoProduto;
+    }
+}
